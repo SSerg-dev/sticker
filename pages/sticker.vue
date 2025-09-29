a4849ab945c466709bda2474fd19e11cf13dd39e
SSerg-dev <srmail@mail.ru>
2025-09-29 16:53:49 -0400

    feat() add fonts Inter, layout scss files

--- pages/sticker.vue
+++ pages/sticker.vue
@@ -2,12 +2,22 @@
   <div>
     <main class="page">
       <h1>Sticker Page</h1>
-      <p>Scroll down — page has filler blocks so you can see the sticker fixed on the right.</p>
+      <p>
+        Scroll down — page has filler blocks so you can see the sticker fixed on
+        the right.
+      </p>
 
-      <section v-for="n in 12" :key="n" style="margin:24px 0; padding:16px; background:#fafafa; border:1px solid #eee;">
-        <h3>Block {{ n }}</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, veritatis.</p>
-        <p v-if="n % 3 === 0">Extra content to make the page longer. Repeat lines. Lorem ipsum…</p>
+      <section class="section__container ">
+        <div  v-for="n in 12" :key="n" class="section__container__item">
+          <h3 class="section__container__item--title">Block {{ n }}</h3>
+          <p class="section__container__item--desc">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis,
+            veritatis.
+          </p>
+          <p v-if="n % 3 === 0" class="section__container__item--desc">
+            Extra content to make the page longer. Repeat lines. Lorem ipsum…
+          </p>
+        </div>
       </section>
     </main>
 
